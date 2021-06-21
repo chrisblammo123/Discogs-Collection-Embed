9a4394b0c027dcea172e61ebffdd7eff47c4eaef
Chris Struck <chrisblammo123@gmail.com>
2021-06-21 11:12:41 -0500

    more setup

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+// Variables
+
+// Node Modules and Constructors
+
+const fs = require('fs');                         // Allows writing to files
+const Discogs = require('disconnect').Client;     // Discogs API from the disconnect node library
+// const database = new Discogs({userToken: "config.token"}).database(); // Creates a new discogs database object with user token auth
+const disc = new Discogs(config.UserAgent, {      // Creates a new discogs object using consumer key and secret for authentication
+  consumerKey: config.consumerKey,
+  consumerSecret: config.consumerSecret
+});
+
+
+
+
+// Local File Includes
+
 // Config files with specific data, such as tokens
 const config = require('./config.json');
 
-// Import the discogs api for node.js
-const Discogs = require('disconnect').Client;
-
-// Creates a new database object 
-const database = new Discogs({userToken: "config.token"}).database();
 
 
-const dis = new Discogs(config.UserAgent);
+
+
+
 
 
 
